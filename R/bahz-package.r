5da636eee6e9001fdfd3a398f10bca7b94bd69f2
Tim Thurman <timothy.j.thurman@gmail.com>
2018-10-06 14:31:34 -0400

    First step: integrating files from rstantools pkg
    Got it to successfully install.

--- R/bahz-package.r
+++ R/bahz-package.r
@@ -1,7 +1,22 @@
-#' BAHZ.
+#' bahz: Bayesian Analysis of Hybrid Zones
 #'
 #' @name bahz-packge
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#' @aliases bahz
+#' @useDynLib bahz, .registration = TRUE
+#'
 #' @docType package
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.4. http://mc-stan.org
+#'
+
+NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 # if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
